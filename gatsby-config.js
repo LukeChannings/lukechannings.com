bcf10dda878d43e64ac5f7472f2ace0f488196d2
Luke Channings <luke@channings.me>
2021-06-10 17:00:46 +0100

    Add 2021-06-09-does-safari-15-fix-the-vh-bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,11 @@
 module.exports = {
   siteMetadata: {
     title: `Luke Channings is a developer.`,
+    titleTemplate: `%s · Luke Channings`,
     author: `Luke Channings`,
     siteUrl: `https://lukechannings.com`,
+    url: `https://lukechannings.com`,
+    twitterUsername: "@LukeChannings",
   },
   flags: {
     FAST_DEV: true,
@@ -12,11 +15,12 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-remark`,
     `gatsby-plugin-typescript`,
+    `gatsby-plugin-mdx`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `blogs`,
-        path: `${__dirname}/blogs`,
+        path: `${__dirname}/blogs/`,
         ignore: ["**/_*"],
       },
     },
@@ -26,60 +30,60 @@ module.exports = {
         outputPath: ".cache/types/gatsby-types.d.ts",
       },
     },
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ "content:encoded": edge.node.html }],
-                })
-              })
-            },
-            query: `
-              {
-                allMarkdownRemark(
-                  sort: { order: DESC, fields: [frontmatter___date] },
-                ) {
-                  edges {
-                    node {
-                      excerpt
-                      html
-                      fields { slug }
-                      frontmatter {
-                        title
-                        date
-                      }
-                    }
-                  }
-                }
-              }
-            `,
-            output: "/rss.xml",
-            title: "Luke Channings",
-            match: "^/blog/",
-          },
-        ],
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-feed`,
+    //   options: {
+    //     query: `
+    //       {
+    //         site {
+    //           siteMetadata {
+    //             title
+    //             description
+    //             siteUrl
+    //             site_url: siteUrl
+    //           }
+    //         }
+    //       }
+    //     `,
+    //     feeds: [
+    //       {
+    //         serialize: ({ query: { site, allMarkdownRemark } }) => {
+    //           return allMarkdownRemark.edges.map(edge => {
+    //             return Object.assign({}, edge.node.frontmatter, {
+    //               description: edge.node.excerpt,
+    //               date: edge.node.frontmatter.date,
+    //               url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+    //               guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+    //               custom_elements: [{ "content:encoded": edge.node.html }],
+    //             })
+    //           })
+    //         },
+    //         query: `
+    //           {
+    //             allMarkdownRemark(
+    //               sort: { order: DESC, fields: [frontmatter___date] },
+    //             ) {
+    //               edges {
+    //                 node {
+    //                   excerpt
+    //                   html
+    //                   fields { slug }
+    //                   frontmatter {
+    //                     title
+    //                     date
+    //                   }
+    //                 }
+    //               }
+    //             }
+    //           }
+    //         `,
+    //         output: "/rss.xml",
+    //         title: "Luke Channings",
+    //         match: "^/blog/",
+    //       },
+    //     ],
+    //   },
+    // },
     `gatsby-plugin-sitemap`,
   ],
 }
---
+++ src/pages/blog/{mdx.parent__(File)__name}.tsx
@@ -0,0 +1,34 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+import { Helmet } from "react-helmet"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+
+export default ({ data }: { data: GatsbyTypes.MDXBlogPageQuery }) => {
+  return (
+    <>
+      <Helmet>
+        <title>
+          {data.mdx?.frontmatter?.title} - {data.mdx?.frontmatter?.author}
+        </title>
+      </Helmet>
+      <div className="wrapper">
+        <main>
+          <h1>{data.mdx?.frontmatter?.title}</h1>
+          <MDXRenderer>{data.mdx?.body}</MDXRenderer>
+        </main>
+      </div>
+    </>
+  )
+}
+
+export const query = graphql`
+  query MDXBlogPage($id: String!) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        author
+      }
+      body
+    }
+  }
+`

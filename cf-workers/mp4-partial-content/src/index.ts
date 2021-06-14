f50b332725d0a6d34a6ce5446109b59872c70737
Luke Channings <luke@channings.me>
2021-06-14 18:41:40 +0100

    Add a Cloudflare worker to fix Safari's MP4 bug

---
+++ cf-workers/mp4-partial-content/src/index.ts
@@ -0,0 +1,50 @@
+async function handleEvent(event: FetchEvent) {
+  const request = event.request
+  const headers = new Map(event.request.headers)
+  if (!event.request.url.endsWith('.mp4')) {
+    return await fetch(request.url)
+  }
+
+  const cacheUrl = new URL(request.url)
+  const cacheKey = new Request(cacheUrl.toString(), request)
+  const cache = caches.default
+  let response = await cache.match(cacheKey)
+  if (!response) {
+    response = await fetch(request)
+    response = new Response(response.body, response)
+    response.headers.append('Cache-Control', 's-maxage=10')
+
+    event.waitUntil(cache.put(cacheKey, response.clone()))
+
+    if (headers.get('range')) {
+      const { from, to } =
+        headers.get('range')?.match(/^bytes=(?<from>[0-9]+)-(?<to>[0-9]*)$/)
+          ?.groups ?? {}
+      const arrayBuffer = await response.arrayBuffer()
+      if (to === '' || Number(to) <= arrayBuffer.byteLength) {
+        const sliced = arrayBuffer.slice(
+          Number(from),
+          Number(to ?? arrayBuffer.byteLength),
+        )
+        return new Response(sliced, {
+          ...response,
+          status: 206,
+          headers: new Headers([
+            ...response.headers,
+            ['content-range', `bytes ${from}-${to}/${arrayBuffer.byteLength}`],
+            ['content-length', String(sliced.byteLength)],
+          ]),
+        })
+      }
+    }
+  }
+  return response
+}
+
+addEventListener('fetch', (event) => {
+  try {
+    return event.respondWith(handleEvent(event))
+  } catch (e) {
+    return event.respondWith(new Response('Error thrown ' + e.message))
+  }
+})
